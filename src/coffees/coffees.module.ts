972f450723cb01781fedb8f705136f45ed3eea31
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-04-04 16:13:52 +0300

    using the config service

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,23 +1,16 @@
-import { Inject, Injectable, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
-import { COFFEE_BRANDS } from './coffees.constants';
-import { Connection } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule],
     controllers: [CoffeesController],
-    providers: [
-        CoffeesService,
-        {
-            provide: COFFEE_BRANDS,
-            useFactory: () => ['buddy brew', 'nescafe']
-        },
-    ],
+    providers: [CoffeesService],
     exports: [CoffeesService],
 })
 export class CoffeesModule {}
