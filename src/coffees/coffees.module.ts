02c5f8c3b0ee0ca22f3807eb9ef29b00960ab93d
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-04-04 12:54:36 +0300

    leverage async providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Inject, Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
@@ -6,25 +6,22 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'typeorm';
 
-@Injectable()
-export class CoffeeBrandsFactory {
-    create() {
-        /* ... do something ... */
-        return ['buddy brew', 'nescafe'];
-    }
-}
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     providers: [
         CoffeesService,
-        CoffeeBrandsFactory,
         {
             provide: COFFEE_BRANDS,
-            useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-                brandsFactory.create(),
-            inject: [CoffeeBrandsFactory],
+            useFactory: async (connection: Connection): Promise<string[]> => {
+                //const coffeeBrands = await connection. .query('SELECT * ...;);
+                const coffeeBrands = await Promise.resolve(['buddy brew', 'nescae']);
+                console.log('[!] Async factory');
+                return coffeeBrands;
+           },
+           inject: [Connection],
         },
     ],
     exports: [CoffeesService],
